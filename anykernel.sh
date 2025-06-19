d6f4747d0193b8b7deacc5374a45f09395e4ff8b
Reina in RainyLand <rainyxeon@gmail.com>
2025-06-19 10:01:17 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,24 +4,27 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Kernel by aryannn999 @ xda-developers
+kernel.string=InfiniR by raystef66 / StarifyBuilder
 do.devicecheck=1
 do.modules=0
-do.systemless=1
+do.systemless=0
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=sweet
-device.name2=sweetin
-supported.versions=11 - 15
+device.name1=cepheus
+device.name2=Cepheus
+device.name3=cepheus-user
+device.name4=Mi 9
+device.name5=Mi9
+supported.versions=
 supported.patchlevels=
-supported.vendorpatchlevels=
 '; } # end properties
 
 
 # boot shell variables
-BLOCK=/dev/block/bootdevice/by-name/boot;
-IS_SLOT_DEVICE=0;
+BLOCK=boot;
+IS_SLOT_DEVICE=1;
 RAMDISK_COMPRESSION=auto;
+PATCH_VBMETA_FLAG=auto;
 
 # import functions/variables and setup patching - see for reference (DO NOT REMOVE)
 . tools/ak3-core.sh;
