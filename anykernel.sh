ff040f14de4915ba0f2e6f990012ae3edd9307b1
RainyXeon <rainyxeon@gmail.com>
2025-12-24 13:06:47 +0700

    Initial commit

--- anykernel.sh
+++ anykernel.sh
@@ -4,29 +4,38 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Kernel by ? @ xda-developers
+kernel.string=Imperial Valeryn by frstprjkt@GitHub
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=ginkgo
-supported.versions=11 - 15
+device.name1=a72q
+supported.versions=
 supported.patchlevels=
 supported.vendorpatchlevels=
 '; } # end properties
 
 
+### AnyKernel install
+## boot files attributes
+boot_attributes() {
+set_perm_recursive 0 0 755 644 $RAMDISK/*;
+set_perm_recursive 0 0 750 750 $RAMDISK/init* $RAMDISK/sbin;
+} # end attributes
+
 # boot shell variables
-BLOCK=boot;
-IS_SLOT_DEVICE=auto;
+BLOCK=/dev/block/bootdevice/by-name/boot;
+IS_SLOT_DEVICE=0;
 RAMDISK_COMPRESSION=auto;
+PATCH_VBMETA_FLAG=auto;
 
 # import functions/variables and setup patching - see for reference (DO NOT REMOVE)
 . tools/ak3-core.sh;
 
 # boot install
-dump_boot;
+dump_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
 
-write_boot;
+write_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
 ## end boot install
+
